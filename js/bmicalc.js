4264b5dc67babb87e12a38bfeb2576df36c69a2b
danny <youchenliang@gmail.com>
2018-03-04 01:57:18 +0800

    complete

--- js/bmicalc.js
+++ js/bmicalc.js
@@ -1,23 +1,20 @@
-const wrap = document.querySelector('.wrap');
+const wrap = document.querySelector('.wrap_record');
 const height = document.querySelector('#height');
 const weight = document.querySelector('#weight');
 const run = document.querySelector('.run');
-const out = document.querySelector('.out')
+const header = document.querySelector('.header');
 let outcomeData = JSON.parse(localStorage.getItem('outcome')) || [];
 
+let stopAdd = false;
 function btnClick() {
-  if (height.value == "" || weight.value == "") {return}
-  calcBMI();
-  run.style.display = 'none';
-  out.style.display = 'flex';
-}
-
-function calcBMI() {
-  // let height = document.querySelector('#height').value;
-  // let weight = document.querySelector('#weight').value;
+  if (height.value == "" || weight.value == "" || stopAdd) {return}
   const bmi = weight.value / ((height.value / 100) * (height.value / 100));
   addOutcome(bmi.toFixed(2), height.value, weight.value);
+
+  run.style.display = 'none';
+  stopAdd = !stopAdd;
 }
+
 run.addEventListener('click', btnClick);
 window.addEventListener('keydown',(e) => {
   if (e.keyCode == 13) {
@@ -33,22 +30,22 @@ function addOutcome(bmi, height, weight) {
   let datatitle;
   if (intbmi >= 35) {
     title = '重度肥胖'
-    datatitle = 5
+    datatitle = 'severe-obese'
   }else if(intbmi >= 30){
     title = '中度肥胖'
-    datatitle = 4
+    datatitle = 'medium-obese'
   }else if(intbmi >= 27){
     title = '輕度肥胖'
-    datatitle = 3
+    datatitle = 'little-obese'
   }else if (intbmi >= 24) {
     title = '過重'
-    datatitle = 2
+    datatitle = 'overweight'
   }else if (intbmi >= 18.5) {
     title = '正常'
-    datatitle = 1
+    datatitle = 'normal'
   }else{
     title = '過輕'
-    datatitle = 0
+    datatitle = 'light'
   }
 
   const data = {
@@ -59,22 +56,21 @@ function addOutcome(bmi, height, weight) {
     height: height,
     date: {
       date: date.getDate(),
-      month: date.getMonth(),
+      month: date.getMonth() + 1,//JANUARY >> 0, FEB >> 1......
       year: date.getFullYear()
     }
   }
   outcomeData.push(data);
   localStorage.setItem("outcome", JSON.stringify(outcomeData));
-  console.log(data.date.month);
 
-  popOutcome(outcomeData)
+  popOutcome(outcomeData);
+  updateBtn(bmi,title,datatitle)
 }
 
 function popOutcome(data) {
-  console.log(data);
   wrap.innerHTML = '<div class="top">BMI紀錄</div>' + data.map(item => {
     return `
-  <div class="outcome" data-title="${item.datatitle}">
+  <div class="outcome" style="border-color: var(--${item.datatitle})">
     <div class="title">${item.title}</div>
     <div class="item">
       <span>BMI </span>${item.bmi}</div>
@@ -90,3 +86,32 @@ function popOutcome(data) {
   }).join('')
 }
 popOutcome(outcomeData);
+
+function updateBtn(bmi,title,datatitle) {
+  const out = document.createElement('div');
+  out.innerHTML = `
+  
+    <div class="bmi">${bmi}</div>
+    <span>BMI</span>
+    <div class="reset" style="background-color: var(--${datatitle})"><span></span></div>
+    <div class="outtitle" style="color: var(--${datatitle})">${title}</div>
+  
+  `
+  out.setAttribute('class','out btn');
+  out.setAttribute('style', `color: var(--${datatitle})`)
+  header.appendChild(out);
+
+  resetBtn();
+}
+
+function resetBtn() {
+  const reset = document.querySelector('.reset');
+  const out = document.querySelector('.out')
+  reset.addEventListener('click',function () {
+    header.removeChild(out);
+    run.style.display = 'flex';
+    height.value = '';
+    weight.value = '';
+    stopAdd = !stopAdd;
+  })
+}
